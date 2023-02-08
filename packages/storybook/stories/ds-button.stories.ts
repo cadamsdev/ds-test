125f4158fe9080573ec5fb6217436442022c1d1e
chadalen <me@chadalen.com>
2023-02-07 20:32:05 -0500

    chore: import lit component to use in storybook

---
+++ packages/storybook/stories/ds-button.stories.ts
@@ -0,0 +1,11 @@
+// demo-button.stories.js
+
+import { html } from 'lit-html';
+
+import '@chadalen/ds-button/dist';
+
+export default {
+  title: 'Button',
+};
+
+export const Primary = () => html`<ds-button>Button 1</ds-button>`;
