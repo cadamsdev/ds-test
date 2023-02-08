305da340e96fc66515acf8f0a5c0f0ef39db06f0
chadalen <me@chadalen.com>
2023-02-07 20:10:55 -0500

    chore: remove tracking dist folder

--- packages/ds-button/dist/button.d.ts
+++
@@ -1,5 +0,0 @@
-import { LitElement, TemplateResult } from 'lit';
-export declare class DSButton extends LitElement {
-    static styles: import("lit").CSSResult;
-    render(): TemplateResult;
-}
--- packages/ds-button/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DSButton = void 0;
-var button_1 = require("./button");
-Object.defineProperty(exports, "DSButton", { enumerable: true, get: function () { return button_1.DSButton; } });
-//# sourceMappingURL=index.js.map
