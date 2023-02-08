a9bd7d59dbb52fde39955a3cf4864cbd9d673ddf
chadalen <me@chadalen.com>
2023-02-07 19:44:19 -0500

    chore: removed npm-workspaces example

---
+++ packages/ds-button/dist/button.d.ts
@@ -0,0 +1,5 @@
+import { LitElement, TemplateResult } from 'lit';
+export declare class DSButton extends LitElement {
+    static styles: import("lit").CSSResult;
+    render(): TemplateResult;
+}
---
+++ packages/ds-button/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DSButton = void 0;
+var button_1 = require("./button");
+Object.defineProperty(exports, "DSButton", { enumerable: true, get: function () { return button_1.DSButton; } });
+//# sourceMappingURL=index.js.map
